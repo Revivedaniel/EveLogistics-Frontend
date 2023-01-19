7436114523999c6a9363392c52fca2d937b991c4
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-18 21:16:11 -0800

    Created button box and each button links to the respective solution

--- eve-logistics-frontend/src/general/SolutionSelect.tsx
+++ eve-logistics-frontend/src/general/SolutionSelect.tsx
@@ -1,5 +1,14 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { Link } from "react-router-dom";
 import css from './SolutionSelect.module.css';
+
+const buttons = [
+    <Button key="one"><Link to={`warehouse`} className={css.link}>Warehouse</Link></Button>,
+    <Button key="two"><Link to={`market`} className={css.link}>Market</Link></Button>,
+    <Button key="three"><Link to={`industry`} className={css.link}>Industry</Link></Button>,
+    <Button key="four"><Link to={`hauling`} className={css.link}>Hauling</Link></Button>,
+  ];
 
 export default function SolutionSelect() {
   return (
@@ -7,6 +16,15 @@ export default function SolutionSelect() {
       <Typography variant="h2" className={css.h2}>
         Solution Select
       </Typography>
+      <Box className={css.solutions}>
+      <ButtonGroup
+        orientation="vertical"
+        aria-label="vertical contained button group"
+        variant="contained"
+      >
+        {buttons}
+      </ButtonGroup>
+    </Box>
     </>
   );
 }
