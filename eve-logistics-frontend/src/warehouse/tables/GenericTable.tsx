f0ceb782014fb47b6cd5561a96a0b0a549006d47
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-20 01:01:08 -0800

    Made the generic table taller

--- eve-logistics-frontend/src/warehouse/tables/GenericTable.tsx
+++ eve-logistics-frontend/src/warehouse/tables/GenericTable.tsx
@@ -2,8 +2,7 @@ import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 
 export default function GenericTable(props: GenericTableProps) {
     return <>
-    <h2>Generic Table</h2>
-    <div style={{ height: 300, width: '100%' }}>
+    <div style={{ height: 500, width: '100%' }}>
       <DataGrid rows={props.rows} columns={props.columns} 
       onSelectionModelChange={(newSelectionModel) => {
         if(props.setSelection) {
