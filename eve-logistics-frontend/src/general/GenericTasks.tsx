fcb94e68cee6f51238b3f1922bd359d5a3592d15
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-20 11:47:15 -0800

    Buttons are optional and small CSS margin update

--- eve-logistics-frontend/src/general/GenericTasks.tsx
+++ eve-logistics-frontend/src/general/GenericTasks.tsx
@@ -7,10 +7,10 @@ import css from './GenericTasks.module.css';
 export default function GenericTasks(props: genericTasksProps) {
   return (
     <>
-      <Typography variant="h2" className={css.h2}>
+      <Typography variant="h2" className={css.h2} style={{marginTop: "25px", marginBottom: "10px"}}>
         {props.heading}
       </Typography>
-      <Box className={css.solutions}>
+      {props.buttons ? <Box className={css.solutions}>
       <ButtonGroup
         orientation="vertical"
         aria-label="vertical contained button group"
@@ -18,7 +18,7 @@ export default function GenericTasks(props: genericTasksProps) {
       >
         {props.buttons.map((button, i) => <Button key={i}><Link to={`${button.to}`} className={css.link}>{button.title}</Link></Button>)}
       </ButtonGroup>
-    </Box>
+    </Box>: null}
     </>
   );
 }
@@ -30,5 +30,5 @@ interface button {
 
 interface genericTasksProps {
   heading: string;
-  buttons: button[];
+  buttons?: button[];
 }
