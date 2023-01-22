25ac8de8c9b85a2c85c88a567461b279d4ffd106
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-22 12:53:05 -0800

    Created System Select component

---
+++ eve-logistics-frontend/src/general/SystemSelect.tsx
@@ -0,0 +1,27 @@
+import aridia from "../utils/systems"
+import { Region, System } from "./General.model"
+import GenericSelect from "./GenericSelect";
+import { useState, useEffect } from 'react';
+
+export default function SystemSelect(props: SystemSelectProps) {
+
+    const [systems, setSystems] = useState<System[] | undefined>(undefined);
+
+    useEffect(() => {
+        if(!systems) {
+          const timer = setTimeout(() => {
+            setSystems(aridia);
+          }, 2500)
+          return () => clearTimeout(timer);
+        }
+      });
+      
+    return <>
+        <GenericSelect title="System" selections={systems} extraButton={{title: `${props.region.name} Inventory`}} setSelection={props.setSystem} />
+    </>
+}
+
+interface SystemSelectProps {
+    region: Region;
+    setSystem: Function;
+}
