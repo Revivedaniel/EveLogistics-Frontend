7a611cd76f4ffaf4771cdefc77e13d416cf8cf00
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-22 13:18:13 -0800

    Created Station Select component

---
+++ eve-logistics-frontend/src/general/StationSelect.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import testStations from '../utils/stations';
+import { Station, System } from "./General.model"
+import GenericSelect from "./GenericSelect";
+
+export default function StationSelect(props: StationSelectProps) {
+
+    const [stations, setStations] = useState<Station[] | undefined>(undefined);
+
+    useEffect(() => {
+        if(!stations) {
+          const timer = setTimeout(() => {
+            setStations(testStations);
+          }, 2500)
+          return () => clearTimeout(timer);
+        }
+      });
+
+    return <>
+        <GenericSelect title="Station" selections={stations} extraButton={{title: `${props.system.name} Inventory`}} setSelection={props.setStation} />
+    </>
+}
+
+interface StationSelectProps {
+    system: System;
+    setStation: Function;
+}
