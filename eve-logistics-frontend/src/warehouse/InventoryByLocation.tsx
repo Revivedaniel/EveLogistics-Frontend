2b1dc047a9dc492d8283579d880b468e099f0b41
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-22 13:18:20 -0800

    Added Station Select

--- eve-logistics-frontend/src/warehouse/InventoryByLocation.tsx
+++ eve-logistics-frontend/src/warehouse/InventoryByLocation.tsx
@@ -1,16 +1,21 @@
 import RegionSelect from "../general/RegionSelect";
 import { useState } from 'react';
-import { Region, System } from "../general/General.model";
+import { Region, Station, System } from "../general/General.model";
 import SystemSelect from "../general/SystemSelect";
+import StationSelect from "../general/StationSelect";
 
 export default function InventoryByLocation() {
 
   const [region, setRegion] = useState<Region | undefined>(undefined);
   const [system, setSystem] = useState<System | undefined>(undefined);
+  const [station, setStation] = useState<Station | undefined>(undefined);
 
   return (
     <>
-      {!region ? <RegionSelect setRegion={setRegion} /> : !system ? <SystemSelect region={region} setSystem={setSystem} /> : <h2>{system.name}</h2>}
+      {!region ? <RegionSelect setRegion={setRegion} /> : 
+      !system ? <SystemSelect region={region} setSystem={setSystem} /> : 
+      !station ? <StationSelect system={system} setStation={setStation} /> : 
+      <h2>{station.name}</h2>}
     </>
   );
 }
