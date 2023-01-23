e491863e3a0527e5998dbd130a47cf47c68695d7
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-22 19:59:42 -0800

    Added Region Inventory

--- eve-logistics-frontend/src/warehouse/InventoryByLocation.tsx
+++ eve-logistics-frontend/src/warehouse/InventoryByLocation.tsx
@@ -6,25 +6,32 @@ import StationSelect from "../general/StationSelect";
 import GenericTasks from "../general/GenericTasks";
 import StationInventory from "./tables/StationInventory";
 import SystemInventory from "./tables/SystemInventory";
+import RegionInventory from "./tables/RegionInventory";
 
 export default function InventoryByLocation() {
 
   const [region, setRegion] = useState<Region | undefined>(undefined);
   const [system, setSystem] = useState<System | undefined>(undefined);
   const [station, setStation] = useState<Station | undefined>(undefined);
-  const [systemInventory, setSystemInventory] = useState<boolean | undefined>(false);
+  const [systemInventory, setSystemInventory] = useState<boolean>(false);
+  const [regionInventory, setRegionInventory] = useState<boolean>(false);
 
   if (system && systemInventory) {
     return <>
       <GenericTasks heading="Inventory by Location" />
       <SystemInventory system={system} setSystemInventory={setSystemInventory} />
     </>
+  } else if (region && regionInventory) {
+    return <>
+      <GenericTasks heading="Inventory by Location" />
+      <RegionInventory region={region} setRegionInventory={setRegionInventory} />
+    </>
   } else {
     return (
       <>
         <GenericTasks heading="Inventory by Location" />
         {!region ? <RegionSelect setRegion={setRegion} /> : 
-        !system ? <SystemSelect region={region} setSystem={setSystem} /> : 
+        !system ? <SystemSelect region={region} setSystem={setSystem} setRegionInventory={setRegionInventory} /> : 
         !station ? <StationSelect system={system} setStation={setStation} setSystemInventory={setSystemInventory}/> : 
         <StationInventory station={station} setStation={setStation} />}
       </>
