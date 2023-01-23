847c3d7b81cda5c92d56c69f0aa69e49b41f681c
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-22 20:08:53 -0800

    Created Universe Inventory component

--- eve-logistics-frontend/src/general/RegionSelect.tsx
+++ eve-logistics-frontend/src/general/RegionSelect.tsx
@@ -3,9 +3,10 @@ import regions from "../utils/regions"
 
 export default function RegionSelect(props: RegionSelectProps) {
 
-    return <GenericSelect title="Region" selections={regions} extraButton={{title: "Universe Inventory"}} setSelection={props.setRegion} />
+    return <GenericSelect title="Region" selections={regions} extraButton={{title: "Universe Inventory"}} setSelection={props.setRegion} setExtraButtonClicked={props.setUniverseInventory} />
 }
 
 interface RegionSelectProps {
     setRegion: Function;
+    setUniverseInventory?: Function;
 }
