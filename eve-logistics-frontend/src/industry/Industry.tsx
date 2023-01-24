56c5e2514f31032cba5aaf24e261cdc15fd893e6
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-23 21:26:19 -0800

    Made non warehouse solutions coming soon

--- eve-logistics-frontend/src/industry/Industry.tsx
+++ eve-logistics-frontend/src/industry/Industry.tsx
@@ -1,28 +1,29 @@
-import GenericTasks from "../general/GenericTasks";
+// import GenericTasks from "../general/GenericTasks";
 
-const buttons = [
-    {
-      to: "current-jobs",
-      title: "Current Jobs"
-    },
-    {
-      to: "new-job",
-      title: "New Job"
-    },
-    {
-      to: "job-history",
-      title: "Job History"
-    },
-    {
-      to: "blueprints",
-      title: "Blueprints"
-    },
-    {
-      to: "new-blueprint",
-      title: "New Blueprint"
-    },
-  ];
+// const buttons = [
+//     {
+//       to: "current-jobs",
+//       title: "Current Jobs"
+//     },
+//     {
+//       to: "new-job",
+//       title: "New Job"
+//     },
+//     {
+//       to: "job-history",
+//       title: "Job History"
+//     },
+//     {
+//       to: "blueprints",
+//       title: "Blueprints"
+//     },
+//     {
+//       to: "new-blueprint",
+//       title: "New Blueprint"
+//     },
+//   ];
 
 export default function Industry() {
-    return <GenericTasks heading="Industry Tasks" buttons={buttons}/>
+    // return <GenericTasks heading="Industry Tasks" buttons={buttons}/>
+    return <h2>Coming Soon...</h2>
 }
