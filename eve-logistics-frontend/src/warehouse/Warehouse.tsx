4ee44a97ad8b542351a9cbecc822fe3622fe9edc
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-23 17:34:11 -0800

    Added Backdrop modal triggered by the Update Inventory button

--- eve-logistics-frontend/src/warehouse/Warehouse.tsx
+++ eve-logistics-frontend/src/warehouse/Warehouse.tsx
@@ -1,3 +1,5 @@
+import { Backdrop } from "@mui/material";
+import { useState } from "react";
 import GenericTasks from "../general/GenericTasks";
 
 const buttons = [
@@ -27,13 +29,27 @@ const buttons = [
   }
 ];
 
-const specialButtons = [
-  {
-    to: "update-inventory",
-    title: "Update Inventory"
-  }
-]
 
 export default function Warehouse() {
-    return <GenericTasks heading="Warehouse Tasks" buttons={buttons} specialButtons={specialButtons} />
+
+  const [updateInventory, setUpdateInventory] = useState<boolean>(false);
+  const handleClose = () => {
+    setUpdateInventory(false);
+  };
+
+    return (
+      <>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={updateInventory}
+          onClick={handleClose}
+        >Updating Inventory...</Backdrop>
+        <GenericTasks
+          heading="Warehouse Tasks"
+          buttons={buttons}
+          specialButtonTitle="Update Inventory"
+          specialButtonSelected={setUpdateInventory}
+        />
+      </>
+    );
 }
