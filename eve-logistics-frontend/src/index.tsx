47655c8fc180c7ec5ff308eb6c4baeaa1e4104ee
Apex <mlg.danielstark@gmail.com>
2023-01-24 16:09:43 -0800

    Quick fix: created route-config.tsx  for routes

--- eve-logistics-frontend/src/index.tsx
+++ eve-logistics-frontend/src/index.tsx
@@ -1,99 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./general/ErrorPage";
-import Warehouse from "./warehouse/Warehouse";
-import Industry from "./industry/Industry";
-import Market from "./market/Market";
-import Hauling from "./hauling/Hauling";
-import GenericTasks from "./general/GenericTasks";
-import InventoryByItem from "./warehouse/InventoryByItem";
-import InventoryByLocation from "./warehouse/InventoryByLocation";
-import VelocityReport from "./warehouse/VelocityReport";
-import Picking from "./warehouse/Picking";
-import Receiving from "./warehouse/Receiving";
-import WarehouseTransfer from "./warehouse/WarehouseTransfer";
-import UpdateInventory from "./warehouse/UpdateInventory";
+import routes from "./route-config";
 
-const buttons = [
-  {
-    to: "warehouse",
-    title: "Warehouse"
-  },
-  {
-    to: "market",
-    title: "market"
-  },
-  {
-    to: "industry",
-    title: "Industry"
-  },
-  {
-    to: "hauling",
-    title: "Hauling"
-  },
-];
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <GenericTasks heading="Solution Select" buttons={buttons} />,
-      },
-      {
-        path: "warehouse",
-        element: <Warehouse />
-      },
-      {
-        path: "warehouse/inventory-by-item",
-        element: <InventoryByItem />
-      },
-      {
-        path: "warehouse/inventory-by-location",
-        element: <InventoryByLocation />
-      },
-      {
-        path: "warehouse/velocity-report",
-        element: <VelocityReport />
-      },
-      {
-        path: "warehouse/picking",
-        element: <Picking />
-      },
-      {
-        path: "warehouse/receiving",
-        element: <Receiving />
-      },
-      {
-        path: "warehouse/warehouse-transfer",
-        element: <WarehouseTransfer />
-      },
-      {
-        path: "warehouse/update-inventory",
-        element: <UpdateInventory />
-      },
-      {
-        path: "industry",
-        element: <Industry />,
-      },
-      {
-        path: "market",
-        element: <Market />,
-      },
-      {
-        path: "hauling",
-        element: <Hauling />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
---
+++ eve-logistics-frontend/src/route-config.tsx
@@ -0,0 +1,93 @@
+import App from "./App";
+import ErrorPage from "./general/ErrorPage";
+import Warehouse from "./warehouse/Warehouse";
+import Industry from "./industry/Industry";
+import Market from "./market/Market";
+import Hauling from "./hauling/Hauling";
+import GenericTasks from "./general/GenericTasks";
+import InventoryByItem from "./warehouse/InventoryByItem";
+import InventoryByLocation from "./warehouse/InventoryByLocation";
+import VelocityReport from "./warehouse/VelocityReport";
+import Picking from "./warehouse/Picking";
+import Receiving from "./warehouse/Receiving";
+import WarehouseTransfer from "./warehouse/WarehouseTransfer";
+import UpdateInventory from "./warehouse/UpdateInventory";
+
+const buttons = [
+    {
+      to: "warehouse",
+      title: "Warehouse"
+    },
+    {
+      to: "market",
+      title: "market"
+    },
+    {
+      to: "industry",
+      title: "Industry"
+    },
+    {
+      to: "hauling",
+      title: "Hauling"
+    },
+  ];
+
+const routes = [
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <GenericTasks heading="Solution Select" buttons={buttons} />,
+            },
+            {
+                path: "warehouse",
+                element: <Warehouse />
+            },
+            {
+                path: "warehouse/inventory-by-item",
+                element: <InventoryByItem />
+            },
+            {
+                path: "warehouse/inventory-by-location",
+                element: <InventoryByLocation />
+            },
+            {
+                path: "warehouse/velocity-report",
+                element: <VelocityReport />
+            },
+            {
+                path: "warehouse/picking",
+                element: <Picking />
+            },
+            {
+                path: "warehouse/receiving",
+                element: <Receiving />
+            },
+            {
+                path: "warehouse/warehouse-transfer",
+                element: <WarehouseTransfer />
+            },
+            {
+                path: "warehouse/update-inventory",
+                element: <UpdateInventory />
+            },
+            {
+                path: "industry",
+                element: <Industry />,
+            },
+            {
+                path: "market",
+                element: <Market />,
+            },
+            {
+                path: "hauling",
+                element: <Hauling />,
+            },
+        ],
+    },
+];
+
+export default routes;
