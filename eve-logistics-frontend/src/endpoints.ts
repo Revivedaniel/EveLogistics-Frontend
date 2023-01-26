8d2b42428f239be63bca42e175fd303da8641677
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-26 01:18:26 -0800

    added /api to all endpoints

--- eve-logistics-frontend/src/endpoints.ts
+++ eve-logistics-frontend/src/endpoints.ts
@@ -1,4 +1,4 @@
-const baseURL = process.env.REACT_APP_API_URL;
+const baseURL = process.env.REACT_APP_API_URL + "/api";
 export const urlItems = `${baseURL}/items`;
 export default function urlItemInventory(itemName: string) {
     return `${baseURL}/inventory?item=${itemName}`;
