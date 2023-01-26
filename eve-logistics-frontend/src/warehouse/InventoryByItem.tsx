17f0505108b154a6f1503681d244ec9931fbccff
Apex <mlg.danielstark@gmail.com>
2023-01-25 18:27:57 -0800

    Inventory by Item retrives a list of items from the backend and renders the inventory for that item when selected

--- eve-logistics-frontend/src/warehouse/InventoryByItem.tsx
+++ eve-logistics-frontend/src/warehouse/InventoryByItem.tsx
@@ -5,34 +5,12 @@ import Inventory from './tables/Inventory';
 import { ItemInventoryRow, ItemTableRow } from './warehouse.model';
 import css from "./tables/ItemInventory.module.css";
 import GenericTasks from '../general/GenericTasks';
-
-// update references to this data when replacing with state
-const rows: ItemTableRow[] = [
-    { id: 0, itemNumber: 34, itemName: 'Tritanium', totalQty: 1216, reservedQty: 500, availableQty: 716 },
-    { id: 1, itemNumber: 36, itemName: 'Mexallon', totalQty: 573, reservedQty: 0, availableQty: 573 },
-    { id: 2, itemNumber: 37, itemName: 'Isogen', totalQty: 4, reservedQty: 1, availableQty: 3 },
-    { id: 3, itemNumber: 39, itemName: 'Zydrine', totalQty: 13, reservedQty: 10, availableQty: 3 },
-    { id: 4, itemNumber: 40, itemName: 'Megacyte', totalQty: 4, reservedQty: 0, availableQty: 4 },
-    { id: 5, itemNumber: 34, itemName: 'Tritanium', totalQty: 1216, reservedQty: 500, availableQty: 716 },
-    { id: 6, itemNumber: 36, itemName: 'Mexallon', totalQty: 573, reservedQty: 0, availableQty: 573 },
-    { id: 7, itemNumber: 37, itemName: 'Isogen', totalQty: 4, reservedQty: 1, availableQty: 3 },
-    { id: 8, itemNumber: 39, itemName: 'Zydrine', totalQty: 13, reservedQty: 10, availableQty: 3 },
-    { id: 9, itemNumber: 40, itemName: 'Megacyte', totalQty: 4, reservedQty: 0, availableQty: 4 },
-    { id: 10, itemNumber: 34, itemName: 'Tritanium', totalQty: 1216, reservedQty: 500, availableQty: 716 },
-    { id: 11, itemNumber: 36, itemName: 'Mexallon', totalQty: 573, reservedQty: 0, availableQty: 573 },
-    { id: 12, itemNumber: 37, itemName: 'Isogen', totalQty: 4, reservedQty: 1, availableQty: 3 },
-    { id: 13, itemNumber: 39, itemName: 'Zydrine', totalQty: 13, reservedQty: 10, availableQty: 3 },
-    { id: 14, itemNumber: 40, itemName: 'Megacyte', totalQty: 4, reservedQty: 0, availableQty: 4 },
-    { id: 15, itemNumber: 34, itemName: 'Tritanium', totalQty: 1216, reservedQty: 500, availableQty: 716 },
-    { id: 16, itemNumber: 36, itemName: 'Mexallon', totalQty: 573, reservedQty: 0, availableQty: 573 },
-    { id: 17, itemNumber: 37, itemName: 'Isogen', totalQty: 4, reservedQty: 1, availableQty: 3 },
-    { id: 18, itemNumber: 39, itemName: 'Zydrine', totalQty: 13, reservedQty: 10, availableQty: 3 },
-    { id: 19, itemNumber: 40, itemName: 'Megacyte', totalQty: 4, reservedQty: 0, availableQty: 4 },
-  ];
+import axios, { AxiosResponse } from 'axios';
+import urlItemInventory, { urlItems } from '../endpoints';
   
   const columns: GridColDef[] = [
-    { field: 'itemNumber', headerName: 'Item Number', width: 150 },
-    { field: 'itemName', headerName: 'Item Name', width: 750 },
+    { field: 'typeId', headerName: 'Item Number', width: 150 },
+    { field: 'name', headerName: 'Item Name', width: 750 },
     { field: 'totalQty', headerName: 'Total Qty', width: 150 },
     { field: 'reservedQty', headerName: 'Reserved', width: 150 },
     { field: 'availableQty', headerName: 'Active', width: 150 },
@@ -60,32 +38,38 @@ const rows: ItemTableRow[] = [
 export default function InventoryByItem() {
 
     const [selection, setSelection] = useState(undefined);
-    const [itemRows, setItemRows] = useState<ItemInventoryRow[] | undefined>(undefined);
+    const [items, setItems] = useState<ItemTableRow[] | undefined>(undefined);
+    const [selectedItem, setSelectedItem] = useState<string>("Selected Item");
+    const [itemInventoryRows, setItemInventoryRows] = useState<ItemInventoryRow[] | undefined>(undefined);
 
   useEffect(() => {
-    if(selection) {
-      const timer = setTimeout(() => {
-        setItemRows(itemrows);
-      }, 2500)
-      return () => clearTimeout(timer);
+    if(selection && items) {
+      setSelectedItem(items[selection[0]].name);
+      axios.get(urlItemInventory(items[selection[0]].name)).then((response: AxiosResponse<ItemInventoryRow[]>) => {
+        setItemInventoryRows(response.data);
+      })
+    } else if (!items) {
+      axios.get(urlItems).then((response: AxiosResponse<ItemTableRow[]>) => {
+        setItems(response.data);
+      })
     }
-  });
+  }, [selection, items]);
 
     return (
       <>
         <GenericTasks heading="Inventory by Item" />
         {selection ? (
           <Inventory
-            rows={itemRows}
+            rows={itemInventoryRows}
             setSelection={setSelection}
-            tableTitle={rows[selection[0]].itemName}
+            tableTitle={selectedItem}
             backButtonTitle="Item Select"
             columns={inventoryItems}
           />
         ) : (
           <div className={css.container}>
             <GenericTable
-              rows={rows}
+              rows={items}
               columns={columns}
               setSelection={setSelection}
             />
