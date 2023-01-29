9de746272ed35e9e9a2a624b000b2e6ecbdabc07
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-28 20:02:38 -0800

    Component fetches data when rendered

--- eve-logistics-frontend/src/warehouse/tables/RegionInventory.tsx
+++ eve-logistics-frontend/src/warehouse/tables/RegionInventory.tsx
@@ -3,100 +3,20 @@ import { StationInventoryRow } from "../warehouse.model";
 import Inventory from "./Inventory";
 import css from "./ItemInventory.module.css";
 import { useState, useEffect } from 'react';
-  
-  const rows: StationInventoryRow[] = [
-    {
-      id: 0,
-      region: "Aridia",
-      system: "Shakasi",
-      station: "Shirshocin III - Moon 2 - Amarr Constructions Warehouse",
-      itemName: "Damage Control II",
-      totalQty: 126,
-      reservedQty: 100,
-      availableQty: 26
-    },
-    {
-      id: 1,
-      region: "Aridia",
-      system: "Shakasi",
-      station: "Shirshocin III - Moon 2 - Amarr Constructions Warehouse",
-      itemName: "Kernite",
-      totalQty: 0,
-      reservedQty: 0,
-      availableQty: 0
-    },
-    {
-      id: 2,
-      region: "Aridia",
-      system: "Shakasi",
-      station: "Shirshocin III - Moon 2 - Amarr Constructions Warehouse",
-      itemName: "Plex",
-      totalQty: 0,
-      reservedQty: 0,
-      availableQty: 0
-    },
-    {
-      id: 3,
-      region: "Aridia",
-      system: "Shakasi",
-      station: "Shirshocin III - Moon 2 - Amarr Constructions Warehouse",
-      itemName: "Tritanium",
-      totalQty: 0,
-      reservedQty: 0,
-      availableQty: 0
-    },
-    {
-        id: 4,
-        region: "Aridia",
-        system: "Shakasi",
-        station: "Shirshocin III - Moon 3 - Amarr Constructions Warehouse",
-        itemName: "Damage Control II",
-        totalQty: 126,
-        reservedQty: 100,
-        availableQty: 26
-      },
-      {
-        id: 5,
-        region: "Aridia",
-        system: "Shakasi",
-        station: "Shirshocin III - Moon 3 - Amarr Constructions Warehouse",
-        itemName: "Kernite",
-        totalQty: 0,
-        reservedQty: 0,
-        availableQty: 0
-      },
-      {
-        id: 6,
-        region: "Aridia",
-        system: "Shakasi",
-        station: "Shirshocin III - Moon 3 - Amarr Constructions Warehouse",
-        itemName: "Plex",
-        totalQty: 0,
-        reservedQty: 0,
-        availableQty: 0
-      },
-      {
-        id: 7,
-        region: "Aridia",
-        system: "Shakasi",
-        station: "Shirshocin III - Moon 3 - Amarr Constructions Warehouse",
-        itemName: "Tritanium",
-        totalQty: 0,
-        reservedQty: 0,
-        availableQty: 0
-      }
-  ]
+import axios, { AxiosResponse } from "axios";
+import { urlRegionInventory } from "../../endpoints";
 
 export default function RegionInventory(props: RegionInventoryProps) {
 
   const [regionInventoryRows, setRegionInventoryRows] = useState<StationInventoryRow[] | undefined>(undefined);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-        setRegionInventoryRows(rows);
-    }, 2500)
-    return () => clearTimeout(timer);
-  });
+    if (!regionInventoryRows) {
+      axios.get(urlRegionInventory(props.region.name)).then((response: AxiosResponse<StationInventoryRow[]>) => {
+        setRegionInventoryRows(response.data);
+      })
+    }
+  }, [regionInventoryRows, props.region.name]);
 
     return <div className={css.container} ><Inventory rows={regionInventoryRows} tableTitle={props.region.name} backButtonTitle="System Select" setSelection={props.setRegionInventory} buttonClickValue={false} /></div>
 }
@@ -104,4 +24,5 @@ export default function RegionInventory(props: RegionInventoryProps) {
 interface RegionInventoryProps {
     region: Region;
     setRegionInventory: Function;
+    inventoryData?: StationInventoryRow[];
 }
