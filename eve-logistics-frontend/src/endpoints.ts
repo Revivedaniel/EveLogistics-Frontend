629118e322c72c08d76d8e59bcbb244e5e2420c9
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-28 20:01:00 -0800

    Added Universe, System, and Station endpoints

--- eve-logistics-frontend/src/endpoints.ts
+++ eve-logistics-frontend/src/endpoints.ts
@@ -1,6 +1,15 @@
 const baseURL = process.env.REACT_APP_API_URL + "/api";
 export const urlItems = `${baseURL}/items`;
-export default function urlItemInventory(itemName: string) {
+export function urlItemInventory(itemName: string) {
     return `${baseURL}/inventory?item=${itemName}`;
 }
-export const urlInventoryItems = `${baseURL}/inventory?`;
+export const urlUniverseInventory = `${baseURL}/inventory`;
+export function urlRegionInventory(regionName: string) {
+    return `${baseURL}/inventory?region=${regionName}`;
+}
+export function urlSystemInventory(systemName: string) {
+    return `${baseURL}/inventory?system=${systemName}`;
+}
+export function urlStationInventory(stationName: string) {
+    return `${baseURL}/inventory?station=${stationName}`;
+}
