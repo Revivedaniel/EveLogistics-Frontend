2a4e23daecab893d886250de314ac0297c088318
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-30 00:04:47 -0800

    Configured AuthenticationContext and Authorized compontent.

--- eve-logistics-frontend/src/App.tsx
+++ eve-logistics-frontend/src/App.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
 import "./App.css";
+import { claim } from "./auth/auth.models";
+import AuthenticationContext from "./auth/AuthenticationContext";
+import Authorized from "./auth/Authorized";
 import Header from "./general/Header";
 
 function App() {
 
+  const [claims, setClaims] = useState<claim[]>([]);
 
   return (
     <>
+    <AuthenticationContext.Provider value={{claims, update: setClaims}} >
+    <Authorized authorized={<>
       <Header />
       <Outlet />
+    </>} notAuthorized={<>You are not authorized</>} role="admin" />
+      
+    </AuthenticationContext.Provider>
     </>
   );
 }
