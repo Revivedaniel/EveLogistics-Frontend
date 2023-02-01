401d435912b9d2fdeb149b94a0711c5736c47cd3
Revivedaniel <mlg.danielstark@gmail.com>
2023-01-31 23:31:19 -0800

    Created a simple login component

---
+++ eve-logistics-frontend/src/auth/Login.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { urlAccounts } from "../endpoints";
+import DisplayErrors from "../utils/DisplayErrors";
+import { authenticationResponse, userCredentials } from "./auth.models";
+import AuthForm from "./AuthForm";
+import {useState} from "react";
+
+export default function Login() {
+
+    const [errors, setErrors] = useState<string[]>([]);
+
+    async function login(credentials: userCredentials) {
+        try {
+            const response = await axios.post<authenticationResponse>(`${urlAccounts}/login`, credentials);
+            console.log(response.data);
+        } catch(error) {
+            setErrors([error.response.data])
+        }
+    }
+
+    return (<>
+        <h3>Login</h3>
+        <DisplayErrors errors={errors} />
+        <AuthForm 
+        model={{email: '', password: ''}} 
+        onSubmit={async values => await login(values)} />
+    </>)
+}
