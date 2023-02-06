593af915733399f6af4ca7f076eadebcc8f71420
Revivedaniel <mlg.danielstark@gmail.com>
2023-02-05 23:40:01 -0800

    Form background and inputs styling started

--- eve-logistics-frontend/src/auth/AuthForm.tsx
+++ eve-logistics-frontend/src/auth/AuthForm.tsx
@@ -7,12 +7,26 @@ export default function AuthForm(props: AuthFormProps) {
     const [emailError, setEmailError] = useState<string>("");
     const [passwordError, setPasswordError] = useState<string>("");
 
+    const textFieldStyles = {
+        "& .MuiFormControl-root": {color: "inherit"},
+        "& Label.MuiFormLabel-root": {color: "inherit"},
+        "& .MuiInputBase-root": {
+            color: "inherit", 
+            backgroundColor: "rgba(10, 10, 10, 0.8);",
+        },
+        "& .MuiOutlinedInput-notchedOutline": {borderColor: "#2e3031"},
+        "&  .Mui-focused fieldset": {borderColor: "#439db4 !important"}, // Using important becase the error prop overwrites any styles
+        "& .MuiInputBase-input": {color: "inherit"},
+        "& .Mui-error fieldset": {borderColor: "#f2884f !important"}, // Using important becase the error prop overwrites any styles
+        "& .MuiFormHelperText-root": {color: "#f2884f !important"},
+    };
+
     function validateEmail(email: string) {
       const re = /\S+@\S+\.\S+/;
       if (!re.test(email)) {
         setEmailError("Invalid email format");
         return;
-      }
+    }
       setEmailError("");
       return;
     }
@@ -44,8 +58,9 @@ export default function AuthForm(props: AuthFormProps) {
           helperText={emailError}
           onBlur={(e) => {validateEmail(e.target.value);}}
           margin="normal"
+          sx={textFieldStyles}
         />
-        <TextField 
+        <TextField
         error={passwordError !== ""}
         required 
         label="Password" 
@@ -54,13 +69,13 @@ export default function AuthForm(props: AuthFormProps) {
         helperText={passwordError} 
         onBlur={(e) => {validatePassword(e.target.value);}}
         margin="normal"
+        sx={textFieldStyles}
         />
 
         <Button type="submit">
           Login
         </Button>
         <Button disabled>Register</Button>
-        <span>Disabled while in alpha</span>
       </form>
     );
 }
--- eve-logistics-frontend/src/auth/Login.tsx
+++ eve-logistics-frontend/src/auth/Login.tsx
@@ -6,7 +6,14 @@ import AuthForm from "./AuthForm";
 import {useState, useContext} from "react";
 import { getClaims, saveToken } from "./handleJWT";
 import AuthenticationContext from "./AuthenticationContext";
-
+import css from "./Login.module.css";
+import loginBackground1 from "../imgs/login-wallpaper-1.jpg";
+import loginBackground2 from "../imgs/login-wallpaper-2.jpg";
+import loginBackground3 from "../imgs/login-wallpaper-3.jpg";
+import loginBackground4 from "../imgs/login-wallpaper-4.jpg";
+import loginBackground5 from "../imgs/login-wallpaper-5.jpg";
+import testBackground from "../imgs/login-wallpaper-6.jpg";
+import tile from "../imgs/tile.jpg";
 export default function Login() {
 
     const [errors, setErrors] = useState<string[]>([]);
@@ -22,11 +29,26 @@ export default function Login() {
         }
     }
 
-    return (<>
-        <h3>Login</h3>
-        <DisplayErrors errors={errors} />
-        <AuthForm 
-        model={{email: '', password: ''}} 
-        onSubmit={async values => await login(values)} />
-    </>)
+    // return (<>
+    //     <h3>Login</h3>
+    //     <DisplayErrors errors={errors} />
+    //     <AuthForm 
+    //     model={{email: '', password: ''}} 
+    //     onSubmit={async values => await login(values)} />
+    // </>)
+    return (
+      <div style={{display: "flex", height: "100vh", backgroundImage: `url(${loginBackground1})`, backgroundSize: "cover"}}>
+        <main className={css.main}>
+            <h1>Eve Logistics</h1>
+            <article>
+                <DisplayErrors errors={errors} />
+                <AuthForm
+                    action={login}
+                />
+          </article>
+        </main>
+        <aside className={css.aside} style={{backgroundImage: `url()`}}>
+        </aside>
+      </div>
+    );
 }
